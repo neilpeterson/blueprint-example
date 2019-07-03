935246682de73648dd5253a37849e4211d10cf45
Neil Peterson <neilpeterson@outlook.com>
2019-07-03 16:37:16 -0700

    Failure Loop

--- assignBlueprint.ps1
+++ assignBlueprint.ps1
@@ -11,7 +11,8 @@
     [string]$SubscriptionId,
     [string]$ManagementGroup,
     [string]$BlueprintName,
-    [string]$BlueprintAssignmentPath
+    [string]$BlueprintAssignmentPath,
+    [bool]$Wait
 )
 
 $BluePrintObject = Get-AzBlueprint -Name $BlueprintName -ManagementGroupId $ManagementGroup
@@ -19,6 +20,22 @@ $BluePrintObject = Get-AzBlueprint -Name $BlueprintName -ManagementGroupId $Mana
 # Add Blueprint ID
 $body = Get-Content -Raw -Path $BlueprintAssignmentPath | ConvertFrom-Json
 $body.properties.blueprintId = $BluePrintObject.id
-$body | ConvertTo-Json | Out-File -FilePath $BlueprintAssignmentPath -Encoding utf8 -Force
+$body | ConvertTo-Json -Depth 4 | Out-File -FilePath $BlueprintAssignmentPath -Encoding utf8 -Force
 
-New-AzBlueprintAssignment -Name "storage-blueprint" -Blueprint $BluePrintObject -AssignmentFile $BlueprintAssignmentPath -SubscriptionId $SubscriptionId
+New-AzBlueprintAssignment -Name "storage-blueprint-fail" -Blueprint $BluePrintObject -AssignmentFile $BlueprintAssignmentPath -SubscriptionId $SubscriptionId
+
+if ($Wait -eq "true") {
+   $timeout = new-timespan -Seconds 240
+   $sw = [diagnostics.stopwatch]::StartNew()
+
+   while (($sw.elapsed -lt $timeout) -or ($AssignemntStatus.ProvisioningState -eq "Succeeded")) {
+         $AssignemntStatus = Get-AzBlueprintAssignment -Name 'storage-blueprint-fail' -SubscriptionId $SubscriptionId
+         write-output $AssignemntStatus.ProvisioningState
+         if ($AssignemntStatus.ProvisioningState -eq "failed") {
+            Write-Error "Issue"
+            break
+         }
+         Sleep 5
+   }
+   $AssignemntStatus = Get-AzBlueprintAssignment -Name 'storage-blueprint-fail' -SubscriptionId $SubscriptionId
+}
